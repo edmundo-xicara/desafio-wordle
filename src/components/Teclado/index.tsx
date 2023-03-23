59fd8a33d698afe87dd003f821feb1b75f0deaf8
Edmundo <edmundopires@outlook.com>
2023-03-23 15:59:52 -0300

    Adição de "Ç" no teclado

--- src/components/Teclado/index.tsx
+++ src/components/Teclado/index.tsx
@@ -12,7 +12,7 @@ export default function Teclado({palavraSecreta, setAlerta}: {palavraSecreta: st
 
     const teclas = [
         ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'], 
-        ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'], 
+        ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ç'], 
         ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', imgApagar]
     ];
 
