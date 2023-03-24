132aaf6eeb394153e6c480a68422e4b4922704e5
Edmundo <edmundopires@outlook.com>
2023-03-24 16:42:35 -0300

    Remoção do "Ç"

--- src/components/Teclado/index.tsx
+++ src/components/Teclado/index.tsx
@@ -12,7 +12,7 @@ export default function Teclado({palavraSecreta, alerta, setAlerta}: {palavraSec
 
     const teclas = [
         ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'], 
-        ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ç'], 
+        ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'], 
         ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', imgApagar]
     ];
 
